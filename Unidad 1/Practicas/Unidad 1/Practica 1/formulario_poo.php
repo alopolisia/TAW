24581dcdc698e4f0c6cbb914bf957eed71b3fabf
alopolisia <1630436@upv.edu.mx>
2019-05-06 09:34:12 -0500

    Se corrige el formulario en POO - 06/Mayo/2019

--- Unidad 1/Practicas/Unidad 1/Practica 1/formulario_poo.php
+++ Unidad 1/Practicas/Unidad 1/Practica 1/formulario_poo.php
@@ -33,6 +33,50 @@ class Solicitud{
         echo "<br>";
         echo $this -> gender;
     }
+
+    //Método para validar los datos de entrada
+    public function validar(){
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (empty($_POST["name"])) {
+                $this-> nameErr = "Name is required";
+            } else {
+                $this-> name = test_input($_POST["name"]);
+                // check if name only contains letters and whitespace
+                if (!preg_match("/^[a-zA-Z ]*$/",$this-> name)) {
+                    $this-> nameErr = "Only letters and white space allowed";
+                }
+            }
+            if (empty($_POST["email"])) {
+                $this-> emailErr = "Email is required";
+            } else {
+                $this-> email = test_input($_POST["email"]);
+                // check if e-mail address is well-formed
+                if (!filter_var($this-> email, FILTER_VALIDATE_EMAIL)) {
+                    $this-> emailErr = "Invalid email format";
+                }
+            }
+            if (empty($_POST["website"])) {
+                $this-> website = "";
+            } else {
+                $this-> website = test_input($_POST["website"]);
+                // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
+                if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$this-> website)) {
+                    $this-> websiteErr = "Invalid URL";
+                }
+            }
+            if (empty($_POST["comment"])) {
+                $this-> comment = "";
+            } else {
+                $this-> comment = test_input($_POST["comment"]);
+            }
+            if (empty($_POST["gender"])) {
+                $this-> genderErr = "Gender is required";
+            } else {
+                $this-> gender = test_input($_POST["gender"]);
+            }
+        }
+    }
+
 }
 
 //Función para limpiar el dato de entrada
@@ -44,75 +88,37 @@ function test_input($data) {
 }
 
 //Se crea el objeto de la clase Solicitud
-$solicitud1 = new Solicitud();
+$solicitud = new Solicitud();
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    if (empty($_POST["name"])) {
-        $solicitud1-> nameErr = "Name is required";
-    } else {
-        $solicitud1-> name = test_input($_POST["name"]);
-        // check if name only contains letters and whitespace
-        if (!preg_match("/^[a-zA-Z ]*$/",$solicitud1-> name)) {
-            $solicitud1-> nameErr = "Only letters and white space allowed";
-        }
-    }
-    if (empty($_POST["email"])) {
-        $solicitud1-> emailErr = "Email is required";
-    } else {
-        $solicitud1-> email = test_input($_POST["email"]);
-        // check if e-mail address is well-formed
-        if (!filter_var($solicitud1-> email, FILTER_VALIDATE_EMAIL)) {
-            $solicitud1-> emailErr = "Invalid email format";
-        }
-    }
-    if (empty($_POST["website"])) {
-        $solicitud1-> website = "";
-    } else {
-        $solicitud1-> website = test_input($_POST["website"]);
-        // check if URL address syntax is valid (this regular expression also allows dashes in the URL)
-        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i",$solicitud1-> website)) {
-            $solicitud1-> websiteErr = "Invalid URL";
-        }
-    }
-    if (empty($_POST["comment"])) {
-        $solicitud1-> comment = "";
-    } else {
-        $solicitud1-> comment = test_input($_POST["comment"]);
-    }
-    if (empty($_POST["gender"])) {
-        $solicitud1-> genderErr = "Gender is required";
-    } else {
-        $solicitud1-> gender = test_input($_POST["gender"]);
-    }
+$solicitud -> validar();
 
-}
 ?>
 
 <h2>PHP Form Validation Example</h2>
 <p><span class="error">* required field.</span></p>
 <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-    Name: <input type="text" name="name" value="<?php echo $solicitud1-> name;?>">
-    <span class="error">* <?php echo $solicitud1-> nameErr;?></span>
+    Name: <input type="text" name="name" value="<?php echo $solicitud-> name;?>">
+    <span class="error">* <?php echo $solicitud-> nameErr;?></span>
     <br><br>
-    E-mail: <input type="text" name="email" value="<?php echo $solicitud1-> email;?>">
-    <span class="error">* <?php echo $solicitud1-> emailErr;?></span>
+    E-mail: <input type="text" name="email" value="<?php echo $solicitud-> email;?>">
+    <span class="error">* <?php echo $solicitud-> emailErr;?></span>
     <br><br>
-    Website: <input type="text" name="website" value="<?php echo $solicitud1-> website;?>">
-    <span class="error"><?php echo $solicitud1-> websiteErr;?></span>
+    Website: <input type="text" name="website" value="<?php echo $solicitud-> website;?>">
+    <span class="error"><?php echo $solicitud-> websiteErr;?></span>
     <br><br>
-    Comment: <textarea name="comment" rows="5" cols="40"><?php echo $solicitud1-> comment;?></textarea>
+    Comment: <textarea name="comment" rows="5" cols="40"><?php echo $solicitud-> comment;?></textarea>
     <br><br>
     Gender:
-    <input type="radio" name="gender" <?php if (isset($solicitud1-> gender) && $solicitud1-> gender=="female") echo "checked";?> value="female">Female
-    <input type="radio" name="gender" <?php if (isset($solicitud1-> gender) && $solicitud1-> gender=="male") echo "checked";?> value="male">Male
-    <span class="error">* <?php echo $solicitud1-> genderErr;?></span>
+    <input type="radio" name="gender" <?php if (isset($solicitud-> gender) && $solicitud-> gender=="female") echo "checked";?> value="female">Female
+    <input type="radio" name="gender" <?php if (isset($solicitud-> gender) && $solicitud-> gender=="male") echo "checked";?> value="male">Male
+    <span class="error">* <?php echo $solicitud-> genderErr;?></span>
     <br><br>
     <input type="submit" name="submit" value="Submit">
 </form>
 
 <?php
     //Imprimir las propiedades
-    $solicitud1 -> imprimir();
+    $solicitud -> imprimir();
 ?>
 
 <ul>
